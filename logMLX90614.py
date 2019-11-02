66b37f32b3bc4e7a9eee54f5501c4df91ec62cbf
zkteh <zkteh@users.noreply.github.com>
2019-11-02 23:05:39 +0800

    all in one main()

--- logMLX90614.py
+++ logMLX90614.py
@@ -7,53 +7,34 @@ import random
 from w1thermsensor import W1ThermSensor
 from datetime import datetime
 
-
 dbname='sensorsData.db'
-#sampleFreq = 1 # time in seconds ==> Sample each 1 min
 
-# get data from DHT sensor
-def getMLXdata():
+def main():
     #mlx90614 calling function	
     i2c = io.I2C(board.SCL, board.SDA, frequency=100000)
     mlx = adafruit_mlx90614.MLX90614(i2c)
 
     #ds18b20 calling function
-    ds18b20_sensor = W1ThermSensor()
-    ds18b20_object = ds18b20_sensor.get_temperature()
-            
-    if mlx and ds18b20_object is not None:     
-        #ds18b20       
-        ds18b20_object = round(ds18b20_object, 2)
-        #mlx90614s
-        mlx_ambient = round(mlx.ambient_temperature, 2)
-        mlx_object = round(mlx.object_temperature, 2)    
-        #logData
-        logData (ds18b20_object, mlx_ambient, mlx_object)
-        return ds18b20_object, mlx_ambient, mlx_object
-    
+    sensor = W1ThermSensor() 
 
-
-# log sensor data on database
-def logData (ds18b20, mlx_ambient, mlx_object):
-
-    #put DB outside of loop to fix some data not able to insert due to deadlock
-    #ref:https://stackoverflow.com/questions/33576296/insert-data-to-mysql-table-every-second-one-time-per-second
+    #initiate DB connection
     conn=sqlite3.connect(dbname)
     curs=conn.cursor()
+            
+    while mlx is not None:
+        #ds18b20 readings
+        ds18b20_object = round(sensor.get_temperature(), 2)        
 
-    while True:
-        curs.execute("INSERT INTO MLX_data VALUES(datetime('now'), ?, ?, ?)",(ds18b20, mlx_ambient, mlx_object))
+        #mlx90614s readings
+        mlx_ambient = round(mlx.ambient_temperature, 2)
+        mlx_object = round(mlx.object_temperature, 2)        
+
+        #DB Insert Data       
+        curs.execute("INSERT INTO MLX_data VALUES(datetime('now'), ?, ?, ?)",(ds18b20_object, mlx_ambient, mlx_object))
         conn.commit()
-        print (str(datetime.now()),"   Data committed: ", "ds18b20: " , ds18b20, "   mlx_ambient: " , mlx_ambient, "   mlx_object: " , mlx_object)
-        time.sleep(1)
+        print (str(datetime.now()),"   Data committed: ", "ds18b20: " , ds18b20_object, "   mlx_ambient: " , mlx_ambient, "   mlx_object: " , mlx_object)
+        #time.sleep(0.5)
 
-    conn.close()
+    conn.close()    
 
-# main function
-def main():
-	while True:                         
-            ds18b20, mlx_ambient, mlx_object = getMLXdata()          
-            logData (ds18b20, mlx_ambient, mlx_object)
-                       
-# ------------ Execute program 
-main()
+main()        
