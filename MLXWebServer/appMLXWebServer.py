e5c1969a1f2b7b6f3230047fa99603ca8f697b89
zkteh <zkteh@users.noreply.github.com>
2019-11-07 16:32:44 +0800

    Update appMLXWebServer.py

--- MLXWebServer/appMLXWebServer.py
+++ MLXWebServer/appMLXWebServer.py
@@ -1,32 +1,41 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
+import json
+import random
+import time
+from datetime import datetime
 import sqlite3
 
+from flask import Flask, Response, render_template
 
-# main route 
-@app.route("/")
+application = Flask(__name__)
+random.seed()  # Initialize the random number generator
+
+
+@application.route('/')
 def index():
-    #initiate DB connection
-    conn=sqlite3.connect('../sensorsData.db')
-    curs=conn.cursor()
+	return render_template('index.html')
 
-    #retrieve data from DB
-    for row in curs.execute("SELECT * FROM MLX_data ORDER BY timestamp DESC LIMIT 1"):
-        timestamp = str(row[0])
-        ds18b20_object = row[1]
-        mlx_ambient = row[2]  
-        mlx_object = row[3]    
-    
-    conn.close()
-        
-    templateData = {
-        'time': timestamp,
-        'ds_temp': ds18b20_object,
-        'mlx_amb': mlx_ambient,
-        'mlx_obj' : mlx_object
-    }
-    return render_template('index.html', **templateData)
-    
+
+
+@application.route('/data')
+def chart_data():
+	def db(database_name='../sensorsData.db'):
+		return sqlite3.connect(database=database_name)
+
+	def query_db(query, args=(), one=False):
+		cur = db().cursor()
+		cur.execute(query, args)
+		r = [dict((cur.description[i][0], value) \
+				for i, value in enumerate(row)) for row in cur.fetchall()]
+		cur.connection.close()
+		return (r[0] if r else None) if one else r
+
+	my_query = query_db("SELECT * FROM MLX_data ORDER BY timestamp DESC LIMIT 1")
+
+	json_output = json.dumps(my_query)
+
+	#print(json_output)
+	return json_output
+
+
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=55555, debug=False)
+   application.run(host='0.0.0.0', port=55555, debug=False)
